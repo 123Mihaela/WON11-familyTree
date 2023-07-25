3aeb7fd9f18900d431a7396846189a0fa9449688
Mihaela <socaciu_mihaela@yahoo.com>
2023-07-25 18:37:14 +0300

    first commit

---
+++ src/main/java/org/fasttrackit/familyTree/FamilyTreeApplication.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.familyTree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyTreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/fasttrackit/familyTree/FamilyTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.familyTree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyTreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
